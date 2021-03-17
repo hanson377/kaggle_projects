b7b407b66a7160a0cf001cdb1e5f6c9793a300a7
josh hanson <hanson377@gmail.com>
2021-03-17 15:45:39 -0600

    uploading latest

---
+++ mens_bb_2021/bayes_pointed_scored.md
@@ -0,0 +1,340 @@
+Simple Bayesian Model for Predicting Tourney Results
+================
+Josh Hanson
+
+------------------------------------------------------------------------
+
+## **Quick Explanation of Model**
+
+------------------------------------------------------------------------
+
+This model will use a simple Bayesian Framework for estimating the
+number of points each team will score in one game. Every team’s scoring
+statistics (2 PT FGs, 3 PT FGs, and FTs scored) will be summarized for
+the entire season to the tournament, as well as statistics related to
+their defensive performance (3 PT FGs, 2 PT FGs, and FTs allowed).
+
+From these summaries, we will use a Bayesian Model that uses an
+opponent’s defensive statistics as our prior for points that a team
+might score. The likelihood will be a team’s offensive statistics. These
+two distributions will then be combined to generate the posterior
+distribution for a team’s total points scored.
+
+From this posterior distribution for points scored, we will generate 1
+million simulations and observe how many times one team outscores the
+other. From these simulations, we can generate probabilistic statements
+on the outcome of a game.
+
+Lastly, we will be modeling points scored with the Gamma/Poisson
+Conjugate Family. Although I’m aware of literature that calls into
+question the efficacy of modeling points scored with a Poisson
+distribution, for the purposes of this exercise, I’m going to assume
+away these questions.
+
+------------------------------------------------------------------------
+
+## **Data Building**
+
+------------------------------------------------------------------------
+
+First, I aggregate our detailed season data by season and team. The
+FUNCTION below provides me with a summary for points scored and points
+allowed by every team and every season in our data. You can find the
+logic for this function in my project folder.
+
+``` r
+summary <- cleanSeasonData(detailed_season_results)[[1]]
+games <- cleanSeasonData(detailed_season_results)[[2]]
+```
+
+I now take the aggregated data and join it to the historical tourney
+results by team and season. This function can also be studied in my
+project folder.
+
+We will generate our historical predictions from this dataset and
+analyze how they compare to the actual observed results.
+
+``` r
+tourney <- seasonToTourney(summary)
+```
+
+------------------------------------------------------------------------
+
+## **Model Example**
+
+------------------------------------------------------------------------
+
+This is all a bit abstract, so lets take a look at a few examples.
+Because I am from Lawrence, Kansas and am a huge Jayhawks fan, I’m going
+to illustrate my model with the 2008 Final Four game between North
+Carolina and Kansas.
+
+First, lets examine the simple model for total 2 PT FGs that Kansas
+might expect to score in the game and compare that to the total 2 PT FGs
+that North Carolina might expect to score in the game. From the simple
+table below, we can see that prior to the post-season, Kansas averaged
+\~23 FGs per game while allowing only 14 FGs per game. North Carolina
+averaged \~26 FGs per game but allowed nearly 20.
+
+We take these simple summary statistics and model a gamma distribution
+for total 2PT FGs scored using the opposing team’s defense (Avg Opp
+FGM2) as the prior and the team’s offensive statistics (Avg FGM2) as the
+likelihood. These are then combined to form a posterior distribution
+that serves as our range of possible points scored by a given team.
+
+<table>
+<caption>
+Simple Summary Statistics: 2 PT FGs Made and Allowed
+</caption>
+<thead>
+<tr>
+<th style="text-align:right;">
+Team ID
+</th>
+<th style="text-align:left;">
+Team Name
+</th>
+<th style="text-align:right;">
+Season
+</th>
+<th style="text-align:right;">
+Total Games Played
+</th>
+<th style="text-align:right;">
+FGM2
+</th>
+<th style="text-align:right;">
+Opponent FGM2
+</th>
+<th style="text-align:right;">
+Avg FGM2
+</th>
+<th style="text-align:right;">
+Avg Opp FGM2
+</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td style="text-align:right;">
+1242
+</td>
+<td style="text-align:left;">
+Kansas
+</td>
+<td style="text-align:right;">
+2008
+</td>
+<td style="text-align:right;">
+33
+</td>
+<td style="text-align:right;">
+748
+</td>
+<td style="text-align:right;">
+467
+</td>
+<td style="text-align:right;">
+22.66667
+</td>
+<td style="text-align:right;">
+14.15152
+</td>
+</tr>
+<tr>
+<td style="text-align:right;">
+1314
+</td>
+<td style="text-align:left;">
+North Carolina
+</td>
+<td style="text-align:right;">
+2008
+</td>
+<td style="text-align:right;">
+34
+</td>
+<td style="text-align:right;">
+895
+</td>
+<td style="text-align:right;">
+671
+</td>
+<td style="text-align:right;">
+26.32353
+</td>
+<td style="text-align:right;">
+19.73529
+</td>
+</tr>
+</tbody>
+</table>
+
+Below, you can find the prior, likelihood, and posterior distributions
+visualized for both teams. You can see that Kansas has a slight edge
+here, despite North Carolina historically scoring many more 2 PT FGs per
+game.
+
+This is because the expectations for Kansas’ very good defense mix with
+the expectations for North Carolina’s very good offense and the result
+is a distribution somewhere in the middle. On the other hand, North
+Carolina’s less impressive defense typically allows somewhere near the
+points Kansas typically scores, meaning our expectations for the
+posterior don’t shift much when predicting Kansas’s total points scored.
+
+From these models, we generate 1 million random draws and observe the
+proportion of times Kansas records more 2 PT FGs than North Carolina.
+When we do this, we find that Kansas records more 2 PT FGs 86% of the
+time. Thus, we can assign an 86% probability to Kansas recording more 2
+PT FGs than North Carolina.
+
+![](bayes_pointed_scored_files/figure-gfm/2%20pt%20fg%20view-1.png)<!-- -->
+
+Interestingly enough, North Carolina recorded 19 2-PT FGs during that
+game, which is a value within the left tail of our predictive posterior
+distribution. Additionally, during the regular season, North Carolina
+saw only 5% of their games with lower 2 PT FGs.
+
+Kansas recorded 29 field goals, which is far above any predicted value
+and a value higher than many of their games during the regular season.
+In fact, Across 33 games, Kansas only recorded 29 or more FG2 \~6% of
+the time.
+
+The above is a great example of why sports are sometimes just a bit
+unpredictable, irregardless of the sophistication of our models. In this
+case, one team performed far above any expectations we might have for
+them while another performed far below any expectations we might
+have…and this was a Final Four game!
+
+As we’ve all heard before…all models are wrong, some are useful. In this
+instance, although the model was wrong with regards to total 2PT FGs
+made, it did successfully predict the binary outcome of one team having
+more 2 PT FGs. That alone is pretty cool.
+
+    ## [1] 0.06060606
+
+![](bayes_pointed_scored_files/figure-gfm/look%20at%20ku%20dist-1.png)<!-- -->
+
+The same exercise is carried out for every type of FG (2 PT, 3 PT, and 1
+PT FT). From our estimates for total FG-types scored, we can estimate
+the total points scored as simply the sum of 2 PT FGs, 3 PT FGS, and FTs
+made. Once we have these estimates for total points scored, we can count
+the number of times one team scores more points than the other across
+our 1 million simulations.
+
+Using this model, we estimate Kansas wins 75% of the time. Indeed, they
+did win.
+
+![](bayes_pointed_scored_files/figure-gfm/points%20scored-1.png)<!-- -->![](bayes_pointed_scored_files/figure-gfm/points%20scored-2.png)<!-- -->
+
+Kansas went onto play Memphis in the title game and win the national
+championship in overtime. What would our models have predicted for this
+game? Our models show this being a clear toss-up, with Memphis winning
+51% of simulations and Kansas winning 49% of simulations.
+
+Digging a little deeper, our model predicts that it is extremely
+unlikely that Kansas would make more 3PT FGs than Memphis, but could
+potentially make up for this difference with free-throws and 3-point
+FGs. This is exactly what we saw, as Memphis made 3 more 3-point FGs
+than Kansas, but 7 less 2 PT FGs and 2 less free-throws.
+
+![](bayes_pointed_scored_files/figure-gfm/kansas%20memphis-1.png)<!-- -->![](bayes_pointed_scored_files/figure-gfm/kansas%20memphis-2.png)<!-- -->
+
+------------------------------------------------------------------------
+
+## **Examination of 2008 NCAA Tournament Results**
+
+------------------------------------------------------------------------
+
+The 2008 Tournament was full of thrilling games and huge upsets. Would
+this model have predicted any of them?
+
+One of the biggest cinderella performances of that year was a run made
+by Davidson. Despite being a 10th seed, they advanced all the way to the
+Sweet 16 before losing to Kansas (the eventual champion). Along the way,
+they defeated some traditionally high-performing programs like Gonzaga,
+Georgetown, and Wisconsin.
+
+Our model would have given Davidson \~55% probability of defeating
+Gonzaga, \~68% probability of defeating Georgetown, and a \~48%
+probability of defeating Wisconsin. It would have then only given
+Davidson an 8% chance of defeating Kansas Jayhawks. All in all, it would
+have nearly perfectly predicted Davidson’s run.
+
+![](bayes_pointed_scored_files/figure-gfm/davidson%20gonzaga-1.png)<!-- -->
+
+![](bayes_pointed_scored_files/figure-gfm/davidson%20georgetown-1.png)<!-- -->
+
+![](bayes_pointed_scored_files/figure-gfm/davidson%20wisconsin-1.png)<!-- -->
+
+![](bayes_pointed_scored_files/figure-gfm/davidson%20kansas-1.png)<!-- -->
+
+Another cinderella story from that year was Western Kentucky. Would this
+modeling approach have predicted their run? Arguably, Western Kentucky
+was mostly the beneficiary of other upsets and beat two rather weak
+teams on their run to the Sweet 16. My model seemed to have picked up on
+that.
+
+Western Kentucky would be given a 55% probability of winning against
+Drake and a 98% probability of winning against San Diego. They did both
+of those things. It would have then given them only \~19% probability of
+beating UCLA. UCLA did indeed go onto defeat Western Kentucky, thus
+ending their cinderella run.
+
+![](bayes_pointed_scored_files/figure-gfm/wku%20drake-1.png)<!-- -->
+
+------------------------------------------------------------------------
+
+![](bayes_pointed_scored_files/figure-gfm/wku%20san%20diego-1.png)<!-- -->
+
+------------------------------------------------------------------------
+
+![](bayes_pointed_scored_files/figure-gfm/wku%20ucla-1.png)<!-- -->
+
+------------------------------------------------------------------------
+
+Although this model seems to have the ability to capture a few upsets,
+it also missed a few. Namely, it would have only given West Virginia a
+25% probability of defeating Duke. Although not horrible odds, a perhaps
+higher than others might have give them, it wouldn’t help me make a
+prediction against Duke.
+
+![](bayes_pointed_scored_files/figure-gfm/duke%20west%20virgina-1.png)<!-- -->
+
+Additionally, it would have given Siena a 45% probability of defeating
+Vanderbilt in the first round. Although this is arguably a toss-up from
+a probabilistic viewpoint, and is perhaps also higher probabilities than
+most would have given them, toss-ups don’t matter when we are using
+these probabilities to define binary predictions.
+
+![](bayes_pointed_scored_files/figure-gfm/siena%20vandy-1.png)<!-- -->
+
+------------------------------------------------------------------------
+
+## **Testing Historical Performance**
+
+------------------------------------------------------------------------
+
+In order to test the true performance of this model, I can take 1000
+random games from previous tournaments and compare my prediction to the
+actual observed result.
+
+Our model successfully predicted the outcome of a game nearly 67% of the
+time. Although not perfect, this isn’t too shabby and is a bit above
+what you might expect from randomly choosing results.
+
+    ## `summarise()` ungrouping output (override with `.groups` argument)
+
+    ## Warning in min(x, na.rm = na.rm): no non-missing arguments to min; returning Inf
+
+    ## Warning in max(x, na.rm = na.rm): no non-missing arguments to max; returning -
+    ## Inf
+
+![](bayes_pointed_scored_files/figure-gfm/summary%20by%20seed%20spread-1.png)<!-- -->
+
+Additionally, the model doesn’t often grossly underestimate a team’s
+probability of winning when they go onto win. A great majority of
+probability estimates for the winning teams are above 50% with very few
+(&lt;13%) below 25%.
+
+![](bayes_pointed_scored_files/figure-gfm/yes-1.png)<!-- -->
---
+++ mens_bb_2021/functions.R
@@ -0,0 +1,280 @@
+## uses opposing behavior as prior
+ModelGen <- function(data) {
+
+## winning stats for points made
+WGames <- max(data$WGames)
+
+ WFGM <- sum(data$WFGM)
+ WFGM3 <- sum(data$WFGM3)
+ WFGM2 <- WFGM-WFGM3
+ WFTM <- sum(data$WFTM)
+
+ ## winning points afford
+ WOppFGM <- sum(data$WOppFGM)
+ WOppFGM3 <- sum(data$WOppFGM3)
+ WOppFGM2 <- WOppFGM-WOppFGM3
+ WOppFTM <- sum(data$WOppFTM)
+
+ WMeanDiff <- max(data$WmeanDiffPoints)
+ WSDDiff <- max(data$WsdDiffPoints)
+
+ ## losing stats scored
+ LGames <- max(data$LGames)
+
+ LFGM <- sum(data$LFGM)
+ LFGM3 <- sum(data$LFGM3)
+ LFGM2 <- LFGM-LFGM3
+ LFTM <- sum(data$LFTM)
+
+ ## losing stats yes
+ LOppFGM <- sum(data$LOppFGM)
+ LOppFGM3 <- sum(data$LOppFGM3)
+ LOppFGM2 <- LOppFGM-LOppFGM3
+ LOppFTM <- sum(data$LOppFTM)
+
+ LMeanDiff <- max(data$LmeanDiffPoints)
+ LSDDiff <- max(data$LsdDiffPoints)
+
+ ## generate models
+ trials <- 500000
+
+ ## general models with gamma distribution
+ WFGM3_posterior = rgamma(trials,(WFGM3+LOppFGM3),(WGames+LGames))
+ WFGM3_prior = rgamma(trials,LOppFGM3,LGames)
+ WFGM3_likelihood = rgamma(trials,WFGM3,WGames)
+
+ WFGM2_posterior = rgamma(trials,(WFGM2+LOppFGM2),(WGames+LGames))
+ WFGM2_prior = rgamma(trials,LOppFGM2,LGames)
+ WFGM2_likelihood = rgamma(trials,WFGM2,WGames)
+
+ WFTM_posterior = rgamma(trials,(WFTM+LOppFTM),(WGames+LGames))
+ WFTM_prior = rgamma(trials,LOppFTM,LGames)
+ WFTM_likelihood = rgamma(trials,WFTM,WGames)
+
+
+ LFGM3_posterior = rgamma(trials,(LFGM3+WOppFGM3),(WGames+LGames))
+ LFGM3_prior = rgamma(trials,WOppFGM3,WGames)
+ LFGM3_likelihood = rgamma(trials,LFGM3,LGames)
+
+ LFGM2_posterior = rgamma(trials,(LFGM2+WOppFGM2),(WGames+LGames))
+ LFGM2_prior = rgamma(trials,WOppFGM2,WGames)
+ LFGM2_likelihood = rgamma(trials,LFGM2,LGames)
+
+ LFTM_posterior = rgamma(trials,(LFTM+WOppFTM),(WGames+LGames))
+ LFTM_prior = rgamma(trials,WOppFTM,WGames)
+ LFTM_likelihood = rgamma(trials,LFTM,LGames)
+
+ WDiff = rnorm(trials,WMeanDiff,WSDDiff)
+ LDiff = rnorm(trials,LMeanDiff,LSDDiff)
+
+ model <- data.frame(WFGM3_posterior,WFGM2_posterior,WFTM_posterior,LFGM3_posterior,LFGM2_posterior,LFTM_posterior,WDiff,LDiff,WFGM3_prior,WFGM3_likelihood,WFGM2_prior,WFGM2_likelihood,LFGM2_likelihood,LFGM2_prior,LFGM3_prior,LFGM3_likelihood,LFTM_prior,LFTM_likelihood,WFTM_prior,WFTM_likelihood,LFTM_prior,LFTM_likelihood)
+
+ model$WPoints <- (WFGM2_posterior*2)+(WFGM3_posterior*3)+(WFTM_posterior*1)
+ model$WPoints_prior <- (WFGM2_prior*2)+(WFGM3_prior*3)+(WFTM_prior*1)
+ model$WPoints_likelihood <- (WFGM2_likelihood*2)+(WFGM3_likelihood*3)+(WFTM_likelihood*1)
+
+ model$LPoints <- (LFGM2_posterior*2)+(LFGM3_posterior*3)+(LFTM_posterior*1)
+ model$LPoints_prior <- (LFGM2_prior*2)+(LFGM3_prior*3)+(LFTM_prior*1)
+ model$LPoints_likelihood <- (LFGM2_likelihood*2)+(LFGM3_likelihood*3)+(LFTM_likelihood*1)
+
+
+ return(model)
+}
+
+
+
+genViews <- function(model,WTeam,LTeam,Year,xmin,xmax,title1,title2) {
+
+  game <- subset(tourney,WTeamName == WTeam & LTeamName == LTeam & Season == Year)
+  simulations <- ModelGen(game)
+
+  if (model == 'FGM2') {
+
+
+    prior <- simulations %>% select(value = WFGM2_prior) %>% mutate(model = 'prior')
+    likelihood <- simulations %>% select(value = WFGM2_likelihood) %>% mutate(model = 'likelihood')
+    posterior <- simulations %>% select(value = WFGM2_posterior) %>% mutate(model = 'posterior')
+
+    sample1 <- rbind(prior,likelihood,posterior)
+    sample1$model <- factor(sample1$model,levels = c('prior','likelihood','posterior'))
+
+
+    prior <- simulations %>% select(value = LFGM2_prior) %>% mutate(model = 'prior')
+    likelihood <- simulations %>% select(value = LFGM2_likelihood) %>% mutate(model = 'likelihood')
+    posterior <- simulations %>% select(value = LFGM2_posterior) %>% mutate(model = 'posterior')
+
+    sample2 <- rbind(prior,likelihood,posterior)
+    sample2$model <- factor(sample2$model,levels = c('prior','likelihood','posterior'))
+
+
+    view1 <- ggplot(sample1,aes(x=value,colour=model)) + geom_density() + coord_cartesian(xlim=c(xmin,xmax)) + theme(legend.position = 'none') + ggtitle(title1)  + xlab('') + ylab('')
+    view2 <- ggplot(sample2,aes(x=value,colour=model)) + geom_density() + coord_cartesian(xlim=c(xmin,xmax)) + theme(legend.position = 'bottom',legend.title=element_blank()) + ggtitle(title2) + xlab('Total 2-Point FGs') + ylab('')
+
+    return(grid.arrange(view1,view2))
+  }
+
+ if (model == 'FGM3') {
+
+   prior <- simulations %>% select(value = WFGM3_prior) %>% mutate(model = 'prior')
+   likelihood <- simulations %>% select(value = WFGM3_likelihood) %>% mutate(model = 'likelihood')
+   posterior <- simulations %>% select(value = WFGM3_posterior) %>% mutate(model = 'posterior')
+
+   sample1 <- rbind(prior,likelihood,posterior)
+   sample1$model <- factor(sample1$model,levels = c('prior','likelihood','posterior'))
+
+
+   prior <- simulations %>% select(value = LFGM3_prior) %>% mutate(model = 'prior')
+   likelihood <- simulations %>% select(value = LFGM3_likelihood) %>% mutate(model = 'likelihood')
+   posterior <- simulations %>% select(value = LFGM3_posterior) %>% mutate(model = 'posterior')
+
+   sample2 <- rbind(prior,likelihood,posterior)
+   sample2$model <- factor(sample2$model,levels = c('prior','likelihood','posterior'))
+
+
+
+   view1 <- ggplot(sample1,aes(x=value,colour=model)) + geom_density() + coord_cartesian(xlim=c(xmin,xmax)) + theme(legend.position = 'none') + ggtitle(title1)  + xlab('') + ylab('')
+   view2 <- ggplot(sample2,aes(x=value,colour=model)) + geom_density() + coord_cartesian(xlim=c(xmin,xmax)) + theme(legend.position = 'bottom',legend.title=element_blank()) + ggtitle(title2) + xlab('Total 3-Point FGs') + ylab('')
+
+   return(grid.arrange(view1,view2))
+ }
+
+ if (model == 'FTM') {
+
+   prior <- simulations %>% select(value = WFTM_prior) %>% mutate(model = 'prior')
+   likelihood <- simulations %>% select(value = WFTM_likelihood) %>% mutate(model = 'likelihood')
+   posterior <- simulations %>% select(value = WFTM_posterior) %>% mutate(model = 'posterior')
+
+   sample1 <- rbind(prior,likelihood,posterior)
+   sample1$model <- factor(sample1$model,levels = c('prior','likelihood','posterior'))
+
+
+   prior <- simulations %>% select(value = LFTM_prior) %>% mutate(model = 'prior')
+   likelihood <- simulations %>% select(value = LFTM_likelihood) %>% mutate(model = 'likelihood')
+   posterior <- simulations %>% select(value = LFTM_posterior) %>% mutate(model = 'posterior')
+
+   sample2 <- rbind(prior,likelihood,posterior)
+   sample2$model <- factor(sample2$model,levels = c('prior','likelihood','posterior'))
+
+
+
+   view1 <- ggplot(sample1,aes(x=value,colour=model)) + geom_density() + coord_cartesian(xlim=c(xmin,xmax)) + theme(legend.position = 'none') + ggtitle(title1) + xlab('') + ylab('')
+   view2 <- ggplot(sample2,aes(x=value,colour=model)) + geom_density() + coord_cartesian(xlim=c(xmin,xmax)) + theme(legend.position = 'bottom',legend.title=element_blank()) + ggtitle(title2) + xlab('Total FTs') + ylab('')
+
+   return(grid.arrange(view1,view2))
+ }
+
+
+  if (model == 'Points') {
+
+    prior <- simulations %>% select(value = WPoints_prior) %>% mutate(model = 'prior')
+    likelihood <- simulations %>% select(value = WPoints_likelihood) %>% mutate(model = 'likelihood')
+    posterior <- simulations %>% select(value = WPoints) %>% mutate(model = 'posterior')
+
+    sample1 <- rbind(prior,likelihood,posterior)
+    sample1$model <- factor(sample1$model,levels = c('prior','likelihood','posterior'))
+
+
+    prior <- simulations %>% select(value = LPoints_prior) %>% mutate(model = 'prior')
+    likelihood <- simulations %>% select(value = LPoints_likelihood) %>% mutate(model = 'likelihood')
+    posterior <- simulations %>% select(value = LPoints) %>% mutate(model = 'posterior')
+
+    sample2 <- rbind(prior,likelihood,posterior)
+    sample2$model <- factor(sample2$model,levels = c('prior','likelihood','posterior'))
+
+    view1 <- ggplot(sample1,aes(x=value,colour=model)) + geom_density() + coord_cartesian(xlim=c(xmin,xmax)) + theme(legend.position = 'none') + ggtitle(title1)  + xlab('') + ylab('')
+    view2 <- ggplot(sample2,aes(x=value,colour=model)) + geom_density() + coord_cartesian(xlim=c(xmin,xmax)) + theme(legend.position = 'bottom',legend.title=element_blank()) + ggtitle(title2) + xlab('Total Points Scored') + ylab('')
+
+    return(grid.arrange(view1,view2))
+  }
+
+}
+
+substrRight <- function(x, n){
+  substr(x, nchar(x)-n+1, nchar(x))
+}
+
+
+cleanSeasonData <- function(data){
+wins <- data %>% select(Season,DayNum,TeamID = WTeamID, OpposingTeamID = LTeamID, FGM = WFGM, FGA = WFGA,OppFGM = LFGM, OppFGA = LFGA, FGM3 = WFGM3, FGA3 = WFGA3, OppFGM3 = LFGM3, OppFGA3 = LFGA3, FTM = WFTM, FTA = WFTA, OppFTM = LFTM, OppFTA = LFTA) %>% mutate(status = 'won')
+
+losses <- data %>% select(Season,DayNum,TeamID = LTeamID, OpposingTeamID = WTeamID,FGM = LFGM, FGA = LFGA, OppFGM = WFGM, OppFGA = WFGA, FGM3 = LFGM3, FGA3 = LFGA3, OppFGM3 = WFGM3, OppFGA3 = WFGA3, FTM = LFTM, FTA = LFTA, OppFTM = WFTM, OppFTA = WFTA) %>% mutate(status = 'lost')
+
+games <- rbind(wins,losses)
+games$diffFG <- (games$FGM-games$OppFGM)
+games$diffFG2 <- (games$FGM-games$FGM3)-(games$OppFGM-games$OppFGM3)
+games$diffFG3 <- (games$FGM3-games$OppFGM3)
+games$diffFT <- (games$FTM-games$OppFTM)
+
+games$diffPoints <- (games$diffFG2*2)+(games$diffFG3*2)+(games$diffFT*1)
+
+summary <- games %>% group_by(TeamID,Season) %>% summarise(total_games = n(), FGM = sum(FGM), FGA = sum(FGA), OppFGM = sum(OppFGM), OppFGA = sum(OppFGA), FGM3 = sum(FGM3), FGA3 = sum(FGA3), OppFGM3 = sum(OppFGM3), OppFGA3 = sum(OppFGA3), FTM = sum(FTM), FTA = sum(FTA), OppFTM = sum(OppFTM), OppFTA = sum(OppFTA), meanDiffPoints = mean(diffPoints), sdDiffPoints = sd(diffPoints))
+
+return(list(summary,games))
+}
+
+seasonToTourney <- function(season_data){
+tourney <- tourney_results %>% select(Season,WTeamID,LTeamID)
+
+summary_wins <- season_data %>%
+  mutate(Season,
+         WTeamID = TeamID,
+         WFGM = FGM,
+         WFGM3 = FGM3,
+         WFGM2 = FGM-FGM3,
+         WFTM = FTM,
+         WOppFGM = OppFGM,
+         WOppFGM3 = OppFGM3,
+         WOppFGM2 = OppFGM-OppFGM3,
+         WOppFTM = OppFTM,
+         WmeanDiffPoints = meanDiffPoints,
+         WsdDiffPoints=sdDiffPoints,
+         WGames = total_games) %>%
+          ungroup() %>%
+            select(Season,WGames,WTeamID,WFGM,WFGM3,WFGM2,WFTM,WOppFGM,WOppFGM3,WOppFGM2,WOppFTM,WmeanDiffPoints,WsdDiffPoints)
+
+summary_losses <- season_data %>%
+  mutate(Season,
+         LTeamID = TeamID,
+         LFGM = FGM,
+         LFGM3 = FGM3,
+         LFGM2 = FGM-FGM3,
+         LFTM = FTM,
+         LOppFGM = OppFGM,
+         LOppFGM3 = OppFGM3,
+         LOppFGM2 = OppFGM-OppFGM3,
+         LOppFTM = OppFTM,
+         LmeanDiffPoints = meanDiffPoints,
+         LsdDiffPoints=sdDiffPoints,
+         LGames = total_games) %>%
+          ungroup() %>%
+            select(Season,LGames,LTeamID,LFGM,LFGM3,LFGM2,LFTM,LOppFGM,LOppFGM3,LOppFGM2,LOppFTM,LmeanDiffPoints,LsdDiffPoints)
+
+tourney <- tourney %>% inner_join(summary_wins,by=c('WTeamID','Season')) %>% inner_join(summary_losses,by=c('LTeamID','Season'))
+tourney <- tourney %>% mutate(game = row_number())
+
+teams <- teams %>% select(TeamID,TeamName)
+tourney <- tourney %>% left_join(teams,by=c('LTeamID' = 'TeamID')) %>% mutate(LTeamName = TeamName) %>% select(-TeamName)
+tourney <- tourney %>% left_join(teams,by=c('WTeamID' = 'TeamID')) %>% mutate(WTeamName = TeamName) %>% select(-TeamName)
+
+return(tourney)
+}
+
+vizPoints <- function(WTeam,LTeam,Year) {
+
+  game <- subset(tourney,WTeamName == WTeam & LTeamName == LTeam & Season == Year)
+  simulations <- ModelGen(game)
+
+  simulations$FGM2_diff <- (simulations$WFGM2_posterior-simulations$LFGM2_posterior)
+  simulations$FGM3_diff <- (simulations$WFGM3_posterior-simulations$LFGM3_posterior)
+  simulations$FTM_diff <- (simulations$WFTM_posterior-simulations$LFTM_posterior)
+  simulations$points_diff <- (simulations$WPoints-simulations$LPoints)
+
+
+  view1 <- ggplot(simulations,aes(x=points_diff)) + geom_histogram(binwidth=1) + ggtitle('Total Points Scored')  + geom_vline(xintercept = 0, linetype = 'dashed', colour='red') + xlab('') + ylab('')
+  view2 <- ggplot(simulations,aes(x=FGM2_diff)) + geom_histogram(binwidth=1)  + ggtitle('FGM2') + geom_vline(xintercept = 0, linetype = 'dashed', colour='red') + xlab('') + ylab('')
+  view3 <- ggplot(simulations,aes(x=FGM3_diff)) + geom_histogram(binwidth=1)  + ggtitle('FGM3') + geom_vline(xintercept = 0, linetype = 'dashed', colour='red') + xlab('') + ylab('')
+  view4 <- ggplot(simulations,aes(x=FTM_diff)) + geom_histogram(binwidth=1) + ggtitle('FTM') + geom_vline(xintercept = 0, linetype = 'dashed', colour='red') + xlab('') + ylab('')
+
+  return(grid.arrange(view1,view2,view3,view4,nrow=2,top = 'Distribution of Differences in Points Scored Across 1M Simulations'))
+
+}
